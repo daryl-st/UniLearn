746f18ebf1e949ce3bd96997fd4d34f0cabc1972
daryl-st <dawit.spark@gmail.com>
2026-03-04 22:37:17 +0300

    Defined common shared types with request and response types

--- packages/shared-types/src/AI.ts
+++ packages/shared-types/src/AI.ts
@@ -1,7 +1,43 @@
+export type Difficulty = "EASY" | "MEDIUM" | "HARD";
+
 export interface SummaryRequest {
     content: string;
+    resourceId: string;
 }
 
 export interface SummaryResponse {
     summary: string;
+}
+
+export interface QuizRequest {
+    difficulty: Difficulty;
+    title: string;
+    resourceId: string;
+}
+
+export interface QuizResponse {
+    difficulty: Difficulty;
+    title: string;
+    resourceId: string;
+    questions: Question[];
+}
+
+export interface Question {
+    content: string;
+    options?: JSON;
+    correctAns: string;
+    quizId: string;
+}
+
+export interface QuizAttempt {
+    score: Number;
+    quizId: string;
+    studentId: string;
+}
+
+export interface Progress {
+    resouceViewed: Number;
+    averageScore: Number; // maybe float here
+    studentId: string;
+    courseId: string;
 }
--- packages/shared-types/src/Course.ts
+++ packages/shared-types/src/Course.ts
@@ -1,6 +1,30 @@
+export type FileType = "PDF" | "PPT" | "DOC"
+
+// for now this can be used to send data to the frontend and as response
 export interface Course {
     id: string;
-    title: string;
-    description: string;
+    Name: string;
+    Code: string;
     instructorId: string;
+    acadamicYear: Number;
+};
+
+export interface UpdateCourseInput {
+    instructorId?: string;
+    acadamicYear?: string;
+};
+
+// same request and response for now
+export interface Resource {
+    title: string;
+    type: FileType;
+    fileUrl: string;
+    courseId: string;
+    instructorId: string;
+};
+
+export interface UpdateResourceInput {
+    title?: string;
+    fileUrl?: string;
+    type?: string;
 }
